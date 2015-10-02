545f976700d3add0ff3cc6ba528ddb13215d0cdd
midhunadarvin <midhunadarvin@gmail.com>
2015-10-02 20:13:55 +0530

    Implemented the Navigation Drawer

--- app/src/main/java/mdplayer/darvin/midhun/mdplayer/MainActivity.java
+++ app/src/main/java/mdplayer/darvin/midhun/mdplayer/MainActivity.java
@@ -1,13 +1,33 @@
 package mdplayer.darvin.midhun.mdplayer;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    String NAME = "Midhun Darvin";
+    String EMAIL = "midhunadarvin@gmail.com";
+    int PROFILE = R.drawable.default_profile_dp;
+
     private Toolbar toolbar;
+    String TITLES[] = {"Home","Profile","Messages","Music","Settings"};
+    int ICONS[] = {R.drawable.ic_home,R.drawable.ic_profile,R.drawable.ic_message,R.drawable.ic_note1,R.drawable.ic_settings};
+
+    RecyclerView mNavigationView;                           // Declaring RecyclerView
+    RecyclerView.Adapter mNavigationAdapter;                        // Declaring Adapter For Recycler View
+    RecyclerView.LayoutManager mLayoutManager;            // Declaring Layout Manager as a linear layout manager
+    DrawerLayout Drawer;
+
+    ActionBarDrawerToggle mDrawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +35,39 @@ public class MainActivity extends AppCompatActivity {
 
         toolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
+
+        mNavigationView = (RecyclerView) findViewById(R.id.RecyclerView); // Assigning the RecyclerView Object to the xml View
+
+        mNavigationView.setHasFixedSize(true);                            // Letting the system know that the list objects are of fixed size
+
+        mNavigationAdapter = new NavAdapter(TITLES,ICONS,NAME,EMAIL,PROFILE);       // Creating the Adapter of MyAdapter class
+                                                                                    // And passing the titles,icons,header view name, header view email,
+                                                                                    // and header view profile picture
+        mLayoutManager = new LinearLayoutManager(this);                 // Creating a layout Manager
+
+        mNavigationView.setLayoutManager(mLayoutManager);               // Setting Layout Manager to Navigation View
+        mNavigationView.setAdapter(mNavigationAdapter);                 // Setting our adapter to Navigation View
+
+
+        Drawer = (DrawerLayout) findViewById(R.id.DrawerLayout);        // Drawer object Assigned to the view
+        mDrawerToggle = new ActionBarDrawerToggle(this,Drawer,toolbar,R.string.nav_open,R.string.nav_close){
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                // code here will execute once the drawer is opened( As I dont want anything happened whe drawer is
+                // open I am not going to put anything here)
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                // Code here will execute once drawer is closed
+            }
+
+        }; // Drawer Toggle Object Made
+        Drawer.setDrawerListener(mDrawerToggle); // Drawer Listener set to the Drawer toggle
+        mDrawerToggle.syncState();               // Finally we set the drawer toggle sync State
     }
 
     @Override
---
+++ app/src/main/java/mdplayer/darvin/midhun/mdplayer/NavAdapter.java
@@ -0,0 +1,130 @@
+package mdplayer.darvin.midhun.mdplayer;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by midhun on 2/10/15.
+ */
+public class NavAdapter extends RecyclerView.Adapter<NavAdapter.ViewHolder>{
+
+    private static final int TYPE_HEADER = 0;  // Declaring Variable to Understand which View is being worked on
+    // IF the view under inflation and population is header or Item
+    private static final int TYPE_ITEM = 1;
+
+    private String mNavTitles[]; // String Array to store the passed titles Value from MainActivity.java
+    private int mIcons[];       // Int Array to store the passed icons resource value from MainActivity.java
+
+    private String name;        //String Resource for header View Name
+    private int profile;        //int Resource for header view profile picture
+    private String email;       //String Resource for header view email
+
+
+    public NavAdapter(String[] titles, int[] icons, String name, String email, int profile) {
+        this.mNavTitles = titles;                //have seen earlier
+        this.mIcons = icons;
+        this.name = name;
+        this.profile = profile;
+        this.email = email;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        int Holderid;
+
+        TextView textView;
+        ImageView imageView;
+        ImageView profile;
+        TextView Name;
+        TextView email;
+
+
+        public ViewHolder(View itemView,int ViewType) {                 // Creating ViewHolder Constructor with View and viewType As a parameter
+            super(itemView);
+
+
+            // Here we set the appropriate view in accordance with the the view type as passed when the holder object is created
+
+            if(ViewType == TYPE_ITEM) {
+                textView = (TextView) itemView.findViewById(R.id.rowText); // Creating TextView object with the id of textView from item_row.xml
+                imageView = (ImageView) itemView.findViewById(R.id.rowIcon);// Creating ImageView object with the id of ImageView from item_row.xml
+                Holderid = 1;                                               // setting holder id as 1 as the object being populated are of type item row
+            }
+            else{
+
+
+                Name = (TextView) itemView.findViewById(R.id.name);         // Creating Text View object from header.xml for name
+                email = (TextView) itemView.findViewById(R.id.email);       // Creating Text View object from header.xml for email
+                profile = (ImageView) itemView.findViewById(R.id.circleView);// Creating Image view object from header.xml for profile pic
+                Holderid = 0;                                                // Setting holder id = 0 as the object being populated are of type header view
+            }
+        }
+    }
+
+
+    @Override
+    public NavAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == TYPE_ITEM) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_row,parent,false); //Inflating the layout
+
+            //Creating ViewHolder and passing the object of type view
+
+            return new ViewHolder(v,viewType); // Returning the created object
+
+            //inflate your layout and pass it to view holder
+
+        } else if (viewType == TYPE_HEADER) {
+
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.header,parent,false); //Inflating the layout
+
+           //Creating ViewHolder and passing the object of type view
+
+            return new ViewHolder(v,viewType); //returning the object created
+
+
+        }
+        return null;
+
+    }
+
+    //Next we override a method which is called when the item in a row is needed to be displayed, here the int position
+    // Tells us item at which position is being constructed to be displayed and the holder id of the holder object tell us
+    // which view type is being created 1 for item row
+
+    @Override
+    public void onBindViewHolder(NavAdapter.ViewHolder holder, int position) {
+        if(holder.Holderid ==1) {                               // as the list view is going to be called after the header view so we decrement the
+                                                                // position by 1 and pass it to the holder while setting the text and image
+            holder.textView.setText(mNavTitles[position - 1]);  // Setting the Text with the array of our Titles
+            holder.imageView.setImageResource(mIcons[position -1]);// Settimg the image with array of our icons
+        }
+        else{
+
+            holder.profile.setImageResource(profile);           // Similarly we set the resources for header view
+            holder.Name.setText(name);
+            holder.email.setText(email);
+        }
+    }
+
+    // This method returns the number of items present in the list
+    @Override
+    public int getItemCount() {
+        return mNavTitles.length+1; // the number of items in the list will be +1 the titles including the header view.
+    }
+
+    // Witht the following method we check what type of view is being passed
+    @Override
+    public int getItemViewType(int position) {
+        if (isPositionHeader(position))
+            return TYPE_HEADER;
+
+        return TYPE_ITEM;
+    }
+
+    private boolean isPositionHeader(int position) {
+        return position == 0;
+    }
+}
