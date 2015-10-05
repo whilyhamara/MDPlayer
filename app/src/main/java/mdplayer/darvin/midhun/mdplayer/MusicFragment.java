dd5ee88d4195d2331b24bf2f0687325a29a50fcf
Midhun A Darvin <midhun.darvin@cabotsolutions.com>
2015-10-05 18:16:20 +0530

    Add list view for songs.

--- app/src/main/java/mdplayer/darvin/midhun/mdplayer/MusicFragment.java
+++ app/src/main/java/mdplayer/darvin/midhun/mdplayer/MusicFragment.java
@@ -1,11 +1,22 @@
 package mdplayer.darvin.midhun.mdplayer;
 
 
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 
 /**
@@ -13,9 +24,19 @@ import android.view.ViewGroup;
  */
 public class MusicFragment extends Fragment {
 
+    Menu menu;
+    private ArrayList<Song> songList;
+    private ListView songListView;
 
     public MusicFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+
     }
 
 
@@ -23,7 +44,55 @@ public class MusicFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_music, container, false);
+        View view = inflater.inflate(R.layout.fragment_music, container, false);
+
+        songListView = (ListView) view.findViewById(R.id.song_list);
+        songList = new ArrayList<Song>();
+
+
+        getSongList(getActivity());
+
+        Collections.sort(songList, new Comparator<Song>() {
+            public int compare(Song a, Song b) {
+                return a.getTitle().compareTo(b.getTitle());
+            }
+        });
+
+        SongAdapter songAdt = new SongAdapter(getActivity(), songList);
+        songListView.setAdapter(songAdt);
+
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.music_menu, menu);
+        this.menu = menu;
+    }
+
+    public void getSongList(Context context) {
+        //retrieve song info
+        ContentResolver musicResolver = context.getContentResolver();
+        Uri musicUri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Cursor musicCursor = musicResolver.query(musicUri, null, null, null, null);
+
+        if(musicCursor!=null && musicCursor.moveToFirst()){
+            //get columns
+            int titleColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media.TITLE);
+            int idColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media._ID);
+            int artistColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media.ARTIST);
+            //add songs to list
+            do {
+                long thisId = musicCursor.getLong(idColumn);
+                String thisTitle = musicCursor.getString(titleColumn);
+                String thisArtist = musicCursor.getString(artistColumn);
+                songList.add(new Song(thisId, thisTitle, thisArtist));
+            }
+            while (musicCursor.moveToNext());
+        }
     }
 
 
---
+++ app/src/main/java/mdplayer/darvin/midhun/mdplayer/Song.java
@@ -0,0 +1,20 @@
+package mdplayer.darvin.midhun.mdplayer;
+
+/**
+ * Created by user on 10/5/2015.
+ */
+public class Song {
+    private long id;
+    private String title;
+    private String artist;
+
+    public Song(long songID, String songTitle, String songArtist) {
+        id=songID;
+        title=songTitle;
+        artist=songArtist;
+    }
+
+    public long getID(){return id;}
+    public String getTitle(){return title;}
+    public String getArtist(){return artist;}
+}
